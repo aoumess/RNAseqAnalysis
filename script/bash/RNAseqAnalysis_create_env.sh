4bc87539514acee961a78a4867001ef44a125628
aoumess <aoumess.zirprus@gmail.com>
2025-02-24 17:34:07 +0100

    . commenting

--- script/bash/RNAseqAnalysis_create_env.sh
+++ script/bash/RNAseqAnalysis_create_env.sh
@@ -9,6 +9,11 @@ INSTALL_ENV_PREFIX="${MAIN_ENV_DIR}/INSTALL"
 ENVRECIPE="${MAIN_ENV_DIR}/env/rnaseq_analysis_env.txt"
 ENVTOOL="micromamba"
 
+## Clone the RNAseqAnalysis repo
+mkdir -p "${PROJDIR}/ENV"
+git clone https://github.com/aoumess/RNAseqAnalysis "${MAIN_ENV_DIR}"
+
+
 ## Create first env (that will be replaced by an explicit pin)
 # "${ENVTOOL}" create --prefix "${CREATE_ENV_PREFIX}" -c conda-forge -c bioconda -c nodefaults -c r --channel-priority disabled r-base rstudio-desktop hunspell-en r-amap r-bigmemory r-ashr r-circlize r-coop r-data.table r-dendextend r-dplyr r-envstats r-ggplot2 r-ggridges r-immunedeconv r-matrixstats r-msigdbr r-nmf r-openxlsx r-purrr r-randomcolor r-remotes r-rsvg r-rcolorbrewer r-vcd r-r.utils r-skmeans r-stringr r-synchronicity r-tibble r-tidyr r-writexl r-writexls r-ggpubr r-glmmTMB r-lmerTest r-pbapply r-pbmcapply r-ggsci r-ggsignif r-ggsurvfit r-survminer r-polynom r-rstatix r-TMB r-reformulas r-broom r-corrplot r-car r-carData r-abind r-pbkrtest r-quantreg r-lme4 r-numDeriv r-doBy r-MatrixModels r-Deriv r-modelr r-microbenchmark r-Rdpack r-rbibutils r-boot r-minqa r-nloptr r-ggfortify r-forcats bioconductor-arrayqualitymetrics bioconductor-annotationdbi bioconductor-biomart  bioconductor-org.hs.eg.db  bioconductor-reactome.db  bioconductor-genomeinfodbdata bioconductor-go.db bioconductor-hdo.db bioconductor-clusterprofiler bioconductor-complexheatmap bioconductor-deseq2 bioconductor-dose bioconductor-enrichplot bioconductor-enrichmentbrowser bioconductor-fgsea bioconductor-go.db bioconductor-gseabase bioconductor-gsva bioconductor-hdo.db bioconductor-ihw bioconductor-limma bioconductor-org.hs.eg.db bioconductor-pathview bioconductor-reactome.db bioconductor-reactomepa bioconductor-sva bioconductor-summarizedexperiment
 ## EXPORT explicit recipe (with md5)
